d6ed6ef79faf95abd1dd132a7e5d4417adbc2c87
Zach Hudson <hudsonzachpro@gmail.com>
2022-06-24 15:55:05 -0600

    Stoplight exercise completed, including bonus

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let stopLight = document.getElementById("stopLight");
+let stopLightClasslist = stopLight.classList;
 
-  // YOUR CODE HERE
-})();
+stopButton.addEventListener("click", () => {
+  stopLight.classList.toggle("stop");
+});
+
+let slowButton = document.getElementById("slowButton");
+let slowLight = document.getElementById("slowLight");
+let slowLightClasslist = slowLight.classList;
+
+slowButton.addEventListener("click", () => {
+  slowLight.classList.toggle("slow");
+});
+
+let goButton = document.getElementById("goButton");
+let goLight = document.getElementById("goLight");
+let goLightClasslist = goLight.classList;
+
+goButton.addEventListener("click", () => {
+  goLight.classList.toggle("go");
+});
+
+[stopButton, slowButton, goButton].forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    console.log(`Entered ${button.innerText} button`);
+  });
+
+  button.addEventListener("mouseleave", () => {
+    console.log(`Left ${button.innerText} button`);
+  });
+});
+
+let controls = document.getElementById("controls");
+controls.addEventListener("click", (event) => {
+  if (event.target === goButton) {
+    if (goLightClasslist.length > 1) {
+      console.log(`${event.target.textContent} bulb is on`);
+    } else {
+      console.log(`${event.target.textContent} bulb is off`);
+    }
+  } else if (event.target === slowButton) {
+    if (slowLightClasslist.length > 1) {
+      console.log(`${event.target.textContent} bulb is on`);
+    } else {
+      console.log(`${event.target.textContent} bulb is off`);
+    }
+  } else {
+    if (stopLightClasslist.length > 1) {
+      console.log(`${event.target.textContent} bulb is on`);
+    } else {
+      console.log(`${event.target.textContent} bulb is off`);
+    }
+  }
+});
